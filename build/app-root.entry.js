4111799b77537a6e6299cb21a377f0b58e5583d4
Raynier van Dam <raynier@e-trailer.nl>
2022-01-28 12:19:07 +0100

    Fixed routing

--- build/app-root.entry.js
+++ build/app-root.entry.js
@@ -7,7 +7,7 @@ let AppRoot = class {
     registerInstance(this, hostRef);
   }
   render() {
-    return (h("div", null, h("header", null, h("i", { class: "fas fa-home" }), h("h1", null, "Property Inspection Form")), h("main", null, h("stencil-router", null, h("stencil-route-switch", { scrollTopOffset: 0 }, h("stencil-route", { url: "/", component: "app-home", exact: true }), h("stencil-route", { url: "/profile/:name", component: "app-profile" }))))));
+    return (h("div", null, h("header", null, h("i", { class: "fas fa-home" }), h("h1", null, "Property Inspection Form")), h("main", null, h("stencil-router", null, h("stencil-route-switch", { scrollTopOffset: 0 }, h("stencil-route", { component: "app-home" }))))));
   }
 };
 AppRoot.style = appRootCss;
